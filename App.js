4ad2919598321840564049fe8ecf06672a7edc69
tsimnujhawj <tsimnuj.hawj@gmail.com>
2019-03-11 01:01:29 -0500

    get request logic added

--- App.js
+++ App.js
@@ -1,17 +1,51 @@
 import React, {Component} from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, Button, FlatList} from 'react-native';
 
 export default class App extends Component
 {
+	constructor(props)
+	{
+		super(props);
+		this.state = {
+
+		}
+	}
+
+	componentDidMount()
+	{
+		fetch("https://jsonplaceholder.typicode.com/users",
+		{
+			method: 'GET',
+			headers: {
+			  Accept: 'application/json',
+			  'Content-Type': 'application/json',
+			}
+		})
+		.then(response => response.json())
+		.then(json =>
+			{
+				this.setState({
+					data: json
+				})
+			}
+		)
+		.catch(err => { console.log(err) })
+	}
+
 	render()
 	{
-		return (
+		const data = this.state.data;
+		return(
 		<View style={styles.container}>
 			<View style={styles.introContainer}>
 				<Text style={styles.introHeader}>LoSkol</Text>
 			</View>
 			<View style={styles.body}>
-				<Text>Body here</Text>
+				<FlatList
+					data = {data}
+					renderItem = {({item}) => <Text style={styles.textStyle}>{item.name}</Text>}
+					keyExtractor={({id}, index) => id.toString()}
+				/>
 			</View>
 		</View>
 		);
@@ -29,7 +63,8 @@ container:
 introHeader:
 {
 	color: "#fff",
-	fontSize: 50
+	fontSize: 80,
+	fontFamily: "NFL_Minnesota_Vikings"
 },
 introContainer:
 {
@@ -37,10 +72,21 @@ introContainer:
 	backgroundColor: "#4F2683",
 	justifyContent: "center",
 	alignItems: "center",
-	alignSelf: "stretch"
+	alignSelf: "stretch",
+	borderBottomColor: "#FFC62F",
+	borderBottomWidth: 5
 },
 body:
 {
-	flex: 1
+	flex: 6,
+	backgroundColor: "#fff",
+	justifyContent: "center",
+	alignItems: "center",
+	alignSelf: "stretch",
+},
+textStyle:
+{
+	color: "black",
+	fontSize: 20
 }
 });
