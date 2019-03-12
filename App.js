2b8ca2dcece226efb4dce7f7477f4eab344aa013
tsimnujhawj <tsimnuj.hawj@gmail.com>
2019-03-11 23:43:05 -0500

    route framwork

--- App.js
+++ App.js
@@ -1,21 +1,125 @@
-import React from "react";
-import { View, Text } from "react-native";
-import { createStackNavigator, createAppContainer } from "react-navigation";
+import React, { Component } from 'react'
+import { Text, View, StyleSheet, Button } from 'react-native'
+import Icon from "react-native-vector-icons/Ionicons"
+import {
+	createSwitchNavigator,
+	createAppContainer,
+	createDrawerNavigator,
+	createBottomTabNavigator,
+	createStackNavigator,
+} from 'react-navigation'
 
-class HomeScreen extends React.Component {
+class App extends Component {
+	render() {
+	  return (
+		<AppContainer />
+	  )
+	}
+  }
+  export default App;
+
+class Welcome extends Component {
   render() {
-    return (
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Text>Home Screen</Text>
-      </View>
-    );
+	return (
+	  <View style={styles.container}>
+		<Text> HomeScreen </Text>
+		<Button title="Login" onPress={()=> this.props.navigation.navigate("Dashboard")} />
+		<Button title="Sign Up" onPress={()=> this.props.navigation.navigate("Dashboard")} />
+	  </View>
+	)
   }
 }
 
-const AppNavigator = createStackNavigator({
-  Home: {
-    screen: HomeScreen
+class Dashboard extends Component {
+	render() {
+	  return (
+		<View style={styles.container}>
+		  <Text> DashboardScreen </Text>
+		</View>
+	  )
+	}
   }
-});
 
-export default createAppContainer(AppNavigator);
+  class Feed extends Component {
+	render() {
+	  return (
+		<View style={styles.container}>
+		  <Text> FeedScreen </Text>
+		</View>
+	  )
+	}
+  }
+
+  class Profile extends Component {
+	render() {
+	  return (
+		<View style={styles.container}>
+		  <Text> ProfileScreen </Text>
+		</View>
+	  )
+	}
+  }
+
+  class Settings extends Component {
+	render() {
+	  return (
+		<View style={styles.container}>
+		  <Text> SettingsScreen </Text>
+		</View>
+	  )
+	}
+  }
+
+const TabNavigator = createBottomTabNavigator({
+	Feed,
+	Profile,
+	Settings
+},
+{
+	navigationOptions: ({navigation})=>
+	{
+		const {routeName} = navigation.state.routes[navigation.state.index]
+		return {
+			headerTitle: routeName
+		}
+	}
+})
+
+const StackNavigator = createStackNavigator({
+	TabNavigator: TabNavigator
+},
+{
+	defaultNavigationOptions: ({navigation}) => {
+		return {
+			headerLeft: (
+				<Icon
+					style={{ paddingLeft: 10 }}
+					onPress={()=> navigation.openDrawer()}
+					name="md-menu"
+					size={30}
+				/>
+			)
+		}
+	}
+}
+)
+
+const AppDrawerNavigator = createDrawerNavigator({
+	Dashboard: { screen: StackNavigator }
+})
+
+const AppSwitchNavigator = createSwitchNavigator({
+	Welcome: { screen: Welcome },
+	Dashboard: { screen: AppDrawerNavigator }
+})
+
+const AppContainer = createAppContainer(AppSwitchNavigator);
+
+const styles = StyleSheet.create({
+container:
+{
+	flex: 1,
+	alignItems: "center",
+	justifyContent: "center",
+}
+})
