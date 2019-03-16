f9ba92f13ba703f8a02d0c10dd20002e4f541aaf
tsimnujhawj <tsimnuj.hawj@gmail.com>
2019-03-16 15:24:50 -0500

    added react-native-firebase with auth dependencies and gradle build setup

--- android/app/src/main/java/com/loskol/MainApplication.java
+++ android/app/src/main/java/com/loskol/MainApplication.java
@@ -3,11 +3,13 @@ package com.loskol;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import io.invertase.firebase.RNFirebasePackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import io.invertase.firebase.auth.RNFirebaseAuthPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -24,7 +26,9 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RNGestureHandlerPackage()
+            new RNFirebasePackage(),
+            new RNGestureHandlerPackage(),
+            new RNFirebaseAuthPackage()
       );
     }
 
